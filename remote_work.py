653836a5a546b3534fab5cf82547b444319391d7
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-02-07 13:20:30 +0100

    changes the interface slightly, obj.method.delay() is now obj.method_delay(). there was a bug where the same bound method was used on different instances, and right now I don't see a better way to get around it. also, it is no longer necessary to pass the class name to the @remote_task decorator

--- remote_work.py
+++ remote_work.py
@@ -40,8 +40,10 @@ try:
     from celery import current_app
 except:
     print "current_app needs to be importable!"
+import new
 
 class_table = dict()
+bound_delay_registry = dict()
 
 class _RemoteWorkerMeta(type):
     instances = dict()
@@ -59,15 +61,15 @@ class _RemoteWorkerMeta(type):
             kwargs.pop("__skip__init", None)
 
             _instance = cls.__new__(cls, *args, **kwargs)
+
             if not __skip__init:
                 _instance.__init__(*args, **kwargs)
 
-            for a_name in filter(lambda a: not a.startswith("__"), dir(_instance)):
+            for a_name in filter(lambda a: not a.startswith("__"), dir(_instance))[:]:
                 attr = getattr(_instance, a_name)
-                if hasattr(attr, "delay"):
-                    _delay = getattr(attr, "delay")
-                    assert isinstance(_delay, _remote_method_delay), "you overwrote the method.delay attribute??"
-                    _delay._instance = _instance
+                if getattr(attr, "__is_remote__", False):
+                    new_name = a_name + "_delay"
+                    setattr(_instance, new_name, new.instancemethod(bound_delay_registry[attr.__task_name__], _instance, cls))
 
             _instance.__instance_id__ = __instance_id__
             cls.instances[(__instance_id__, cls)] = (_instance, (args, kwargs))
@@ -81,7 +83,7 @@ def RemoteWorker(cls):
     __bases = tuple(cls.__bases__)
     __dict = dict(cls.__dict__)
     for each_slot in __dict.get("__slots__", tuple()):
-            __dict.pop(each_slot, None)
+        __dict.pop(each_slot, None)
 
     __dict["__metaclass__"] = _RemoteWorkerMeta
     __dict["__wrapped__"] = cls
@@ -99,31 +101,65 @@ class _remote_method_delay(object):
     def __call__(self, *args, **kwargs):
         __instance_id__ = self._instance.__instance_id__
         _, instantiation_args = self._instance.__class__.instances[(__instance_id__, self._instance.__class__)]
-        kwargs.update({"__instance_id__": __instance_id__, "__instantiation_args__": instantiation_args})
+        kwargs.update({
+            "__class_name__": self._instance.__class__.__name__,
+            "__instance_id__": __instance_id__,
+            "__instantiation_args__": instantiation_args
+        })
         res = current_app.send_task(self.task_name, args, kwargs)
         return res
 
 
-def remote_task(class_name, task_name_suffix=None):
+
+def remote_task(*decorator_args, **decorator_kwargs):
+    no_args = False
+    if len(decorator_args) == 1 and not decorator_kwargs and callable(decorator_args[0]):
+        # We were called without args
+        method_ = decorator_args[0]
+        no_args = True
+
+    task_name_ = decorator_kwargs.get("task_name", None)
+
     def remote_task_decorator(method):
         method_name = method.__name__
-        task_name = "%s.%s" % (class_name, task_name_suffix or method_name)
+        task_name = task_name_ or method_name
 
         @current_app.task(name=task_name)
         def method_using_local_class_instance(*args, **kwargs):
+            __class_name__ = kwargs["__class_name__"]
             __instance_id__ = kwargs["__instance_id__"]
             r_args, r_kwargs = kwargs["__instantiation_args__"]
+            del kwargs["__class_name__"]
             del kwargs["__instance_id__"]
             del kwargs["__instantiation_args__"]
 
-            method_class = class_table[class_name]
+            method_class = class_table[__class_name__]
             _instance = method_class(*r_args, __instance_id__=__instance_id__, __skip__init=False, **r_kwargs)
             actual_local_method = getattr(_instance, method_name)
             return actual_local_method(*args, **kwargs)
 
-        # this may look like it can't work, but the instance reference get's supplied to the delay function later on.
-        method.delay = _remote_method_delay(task_name)
+        def method_delay(self, *args, **kwargs):
+            __instance_id__ = self.__instance_id__
+            _, instantiation_args = self.__class__.instances[(__instance_id__, self.__class__)]
+            kwargs.update({
+                "__class_name__": self.__class__.__name__,
+                "__instance_id__": __instance_id__,
+                "__instantiation_args__": instantiation_args
+            })
+            res = current_app.send_task(task_name, args, kwargs)
+            return res
+
+        #method.__delay = method_delay
+        method.__task_name__ = task_name
+        method.__is_remote__ = True
+
+        bound_delay_registry[task_name] = method_delay
 
         return method
-    return remote_task_decorator
+
+    if no_args:
+        return remote_task_decorator(method_)
+    else:
+        return remote_task_decorator
+
 
