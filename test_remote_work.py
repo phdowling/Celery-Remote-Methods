d845c33c20d763d3be1655ad00806adcd68b8673
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-02-07 14:03:03 +0100

    show better example

--- test_remote_work.py
+++ test_remote_work.py
@@ -3,7 +3,7 @@ app = Celery('tasks', backend='amqp', broker='amqp://')
 
 from remote_work import RemoteWorker, remote_task
 
-bad_adder = None
+bad_adder, nbad_adder = None, None
 
 
 @RemoteWorker
@@ -22,14 +22,27 @@ class BadAdder(object):
     def sub_badly(self, x, y):
         return x + y + self.error
 
+@RemoteWorker
+class NegativeBadAdder(object):
+    def __init__(self, error=0):
+        print "Instantiated!"
+        self.error = error
+
+    # example for when a task name is necessary. caller side, we still use obj.add_badly_delay!
+    @remote_task(task_name="neg_add_badly")
+    def add_badly(self, x, y):
+        return x + y - self.error
 
 def test():
-    global bad_adder
+    global bad_adder, nbad_adder
 
     # create an instance. Note that this will not cause the __init__ method to be called locally,
     # only in the worker when we first call bad_adder.delay(...)
     bad_adder = bad_adder or BadAdder(10)
+    nbad_adder = nbad_adder or NegativeBadAdder(10)
 
     async_res = bad_adder.add_badly_delay(1, 2)
+    async_res2 = nbad_adder.add_badly_delay(1, 2)
     async_res.get()
-    print async_res.result
+    async_res2.get()
+    print async_res.result, async_res2.result
